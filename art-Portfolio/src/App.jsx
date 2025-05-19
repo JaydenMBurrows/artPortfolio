44b95329a864c398168def82743ec4a581cd834a
JaydenMBurrows <jaydenborrows@gmail.com>
2025-05-19 19:54:19 -0400

    added one image and fixed another

--- art-Portfolio/src/App.jsx
+++ art-Portfolio/src/App.jsx
@@ -3,13 +3,14 @@ import Card from './Card.jsx'
 import ballerinaGuy from '../src/assets/ballerinaGuy.png'
 import blueWaterColorGuy from '../src/assets/blueWaterColorGuy.png'
 import bellaSunlight from '../src/assets/bellaSunlight.png'
+import bellaGrass from '../src/assets/bellaGrass.png'
 import davidMazousTux from '../src/assets/davidMazousTux.png'
 import davidMazousSerious from '../src/assets/davidMazousSerious.png'
 import funnyDavidMazous from '../src/assets/funnyDavidMazous.png'
 import itClown from '../src/assets/itClown.png'
 import pedroPascal from '../src/assets/pedroPascal.png'
 import trashRaccoon from '../src/assets/trashRaccoon.png'
-import astronaut from '../src/assets/ballerinaGuy.png'
+import astronaut from '../src/assets/astronaut.png'
 import gooseWallpaper from '../src/assets/astronaut.png'
 import flowerPhoto from '../src/assets/flowerPhoto.png'
 import sky from '../src/assets/sky.png'
@@ -27,6 +28,7 @@ function App() {
         <Card url={ballerinaGuy} desc="Sketch of a man done in pencil"/>
         <Card url={blueWaterColorGuy} desc="Painting of man done in watercolor"/>
         <Card url={bellaSunlight} desc="Painting of dog done in watercolor"/>
+        <Card url={bellaGrass} desc="Painting of dog done in watercolor"/>
         <Card url={davidMazousTux} desc="A sketch of David Mazous smiling"/>
         <Card url={davidMazousSerious} desc="A sketch of David Mazous as Bruce Wayne"/>
         <Card url={funnyDavidMazous} desc="A sketch of David Mazous making a weird expression"/>
